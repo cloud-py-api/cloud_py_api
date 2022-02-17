30db2194816b552a27bc7be3e4e1d54140901eb9
Alexander Piskun <bigcat88@icloud.com>
2022-02-17 17:58:31 +0200

    dev

--- build_tools/docker/test_self_install/debian.sh
+++ build_tools/docker/test_self_install/debian.sh
@@ -3,7 +3,6 @@
 ECHO_LINE_BREAK="echo -----------------------------------------------------"
 FRM_CONFIG="%7B%22loglvl%22%3A%22DEBUG%22%2C%22frmAppData%22%3A%22%2Fcloud_py_api%22%2C%22dbConfig%22%3A%7B%22dbType%22%3A%22pgsql%22%2C%22dbUser%22%3A%22%22%2C%22dbName%22%3A%22nextcloud%22%7D%7D"
 
-set -ex && ls -la . && ls -la host/home/runner/work/cloud_py_api/cloud_py_api/pyfrm
 cp -r host/home/runner/work/cloud_py_api/cloud_py_api/pyfrm /
 # cp -r /var/www/nextcloud/apps/cloud_py_api/pyfrm /
 if [ -d "/cloud_py_api/st_python" ]; then
