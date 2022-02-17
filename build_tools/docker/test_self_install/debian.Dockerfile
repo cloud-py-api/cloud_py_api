0b6316ff08bfc031b02325814b27f32c774cb449
Alexander Piskun <bigcat88@icloud.com>
2022-02-17 17:28:59 +0200

    dev

--- build_tools/docker/test_self_install/debian.Dockerfile
+++ build_tools/docker/test_self_install/debian.Dockerfile
@@ -9,11 +9,12 @@ ENV SP_AMD64="$SP_BASE_URL-x86_64-unknown-linux-gnu-lto-20211017T1616.tar.zst"
 ENV SP_ARM64="$SP_BASE_URL-aarch64-unknown-linux-gnu-lto-20211017T1616.tar.zst"
 ENV WGET_CMD="wget -q --no-check-certificate -O standalone.tar.zst"
 
-RUN set -ex && apt update && apt install -y python3-minimal zstd wget && chmod +x /entrypoint.sh && python3 -V
+RUN mkdir /cloud_py_api
+RUN set -ex && apt update && apt install -y python3-minimal zstd wget sudo && chmod +x /entrypoint.sh && python3 -V
 ARG TARGETARCH
 RUN if [ "$TARGETARCH" = "amd64" ] ; then $WGET_CMD $SP_AMD64 ; else $WGET_CMD $SP_ARM64 ; fi
 RUN zstd -d standalone.tar.zst && tar xf standalone.tar && rm standalone.tar standalone.tar.zst
-RUN mv python/install st_python && rm -rf python
-RUN ./st_python/bin/python3 -V
+RUN mv python/install /cloud_py_api/st_python && rm -rf python
+RUN ./cloud_py_api/st_python/bin/python3 -V
 
 CMD ["sh", "-c", "/entrypoint.sh"]
--- build_tools/docker/test_self_install/debian.sh
+++ build_tools/docker/test_self_install/debian.sh
@@ -1,4 +1,33 @@
 #!/bin/sh
 
-set -ex && ls -la . && ls -la host
+ECHO_LINE_BREAK="echo -----------------------------------------------------"
+FRM_CONFIG="%7B%22loglvl%22%3A%22DEBUG%22%2C%22frmAppData%22%3A%22%2Fcloud_py_api%22%2C%22dbConfig%22%3A%7B%22dbType%22%3A%22pgsql%22%2C%22dbUser%22%3A%22%22%2C%22dbName%22%3A%22nextcloud%22%7D%7D"
+
+set -ex && ls -la . && ls -la host/home/runner/work/cloud_py_api/cloud_py_api/pyfrm
+cp -r host/home/runner/work/cloud_py_api/cloud_py_api/pyfrm /
+# cp -r /var/www/nextcloud/apps/cloud_py_api/pyfrm /
+if [ -d "/cloud_py_api/st_python" ]; then
+  echo "Python standalone present, checking on it."
+  if ! /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
+    $ECHO_LINE_BREAK && echo "Installing" && $ECHO_LINE_BREAK
+    /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
+  fi
+elif ! python3 -V; then
+  echo "System or Standalone python not found. Test failed." && exit 102
+fi
+if python3 -V; then
+#  echo "Checking on system python as user."
+#  if ! python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
+#    $ECHO_LINE_BREAK && echo "Installing" && $ECHO_LINE_BREAK
+#    python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
+#  fi
+  rm -rf /cloud_py_api/local
+  echo "Checking on system python as root."
+  if ! python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
+    $ECHO_LINE_BREAK && echo "Installing" && $ECHO_LINE_BREAK
+    python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
+  fi
+  rm -rf /cloud_py_api/local
+fi
+echo "Test OK."
 exit 0
