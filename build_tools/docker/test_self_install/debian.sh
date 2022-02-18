deb0457f2b2459def4c71f6b425b32f8ddc0f241
Alexander Piskun <bigcat88@icloud.com>
2022-02-18 23:58:02 +0200

    dev

--- build_tools/docker/test_self_install/debian.sh
+++ build_tools/docker/test_self_install/debian.sh
@@ -2,36 +2,48 @@
 
 ECHO_LINE_BREAK="echo -----------------------------------------------------"
 FRM_CONFIG="%7B%22loglvl%22%3A%22DEBUG%22%2C%22frmAppData%22%3A%22%2Fcloud_py_api%22%2C%22dbConfig%22%3A%7B%22dbType%22%3A%22pgsql%22%2C%22dbUser%22%3A%22%22%2C%22dbName%22%3A%22nextcloud%22%7D%7D"
-DUMP_FOLDERS="ls -la /cloud_py_api /cloud_py_api/.local /var/www"
+DUMP_FOLDERS="ls -la /cloud_py_api /cloud_py_api/.local /cloud_py_api/st_python /var/www"
 CLEAR_FOLDERS="rm -rf /cloud_py_api/.local"
+AS_USER="sudo -u www-data"
+G_PACKAGES_INSTALL="apt install python3-pip && pip3 install pipdeptree pg8000 PyMySQL protobuf SQLAlchemy"
 
 cp -r host/home/runner/work/cloud_py_api/cloud_py_api/pyfrm /
 if [ -d "/cloud_py_api/st_python" ]; then
-  echo "Python standalone present, checking on it."
-  if ! /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
+  $ECHO_LINE_BREAK && echo "Standalone python(user): checking." && $ECHO_LINE_BREAK
+  if ! $AS_USER /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
     $DUMP_FOLDERS
-    $ECHO_LINE_BREAK && echo "Installing" && $ECHO_LINE_BREAK
-    /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
+    $ECHO_LINE_BREAK && echo "Standalone python(user): installing" && $ECHO_LINE_BREAK
+    $AS_USER /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
   fi
   $DUMP_FOLDERS
   $CLEAR_FOLDERS
 elif ! python3 -V; then
-  echo "System or Standalone python not found. Test failed." && exit 102
+  echo "ERROR! System or Standalone python not found. Test failed." && exit 102
 fi
 if python3 -V; then
-  $ECHO_LINE_BREAK && echo "Checking on system python as user." && $ECHO_LINE_BREAK
-  if ! sudo -H -u www-data python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
+  $ECHO_LINE_BREAK && echo "Sys python(user): checking." && $ECHO_LINE_BREAK
+  if ! $AS_USER python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
     $DUMP_FOLDERS
-    $ECHO_LINE_BREAK && echo "Installing" && $ECHO_LINE_BREAK
-    sudo -H -u www-data python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
+    $ECHO_LINE_BREAK && echo "Sys python(user): installing." && $ECHO_LINE_BREAK
+    $AS_USER python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
   fi
   $DUMP_FOLDERS
   $CLEAR_FOLDERS
-  $ECHO_LINE_BREAK && echo "Checking on system python as root." && $ECHO_LINE_BREAK
+  $ECHO_LINE_BREAK && echo "Sys python(root): checking." && $ECHO_LINE_BREAK
   if ! python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
     $DUMP_FOLDERS
-    $ECHO_LINE_BREAK && echo "Installing" && $ECHO_LINE_BREAK
+    $ECHO_LINE_BREAK && echo "Sys python(root): installing." && $ECHO_LINE_BREAK
     python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
+  fi
+  $DUMP_FOLDERS
+  $CLEAR_FOLDERS
+  $ECHO_LINE_BREAK && echo "Installing packages globally" && $ECHO_LINE_BREAK
+  $G_PACKAGES_INSTALL
+  $ECHO_LINE_BREAK && echo "Sys python w packages(user): checking." && $ECHO_LINE_BREAK
+  if ! $AS_USER python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
+    $DUMP_FOLDERS
+    $ECHO_LINE_BREAK && echo "Sys python w packages(user): installing." && $ECHO_LINE_BREAK
+    $AS_USER python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
   fi
   $DUMP_FOLDERS
   $CLEAR_FOLDERS
