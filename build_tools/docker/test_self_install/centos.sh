2c2ae190c0467109ea9caf688b623cebc17e78c3
Alexander Piskun <bigcat88@icloud.com>
2022-02-19 01:48:07 +0200

    dev

--- build_tools/docker/test_self_install/centos.sh
+++ build_tools/docker/test_self_install/centos.sh
@@ -2,18 +2,20 @@
 
 ECHO_LINE_BREAK="echo -----------------------------------------------------"
 FRM_CONFIG="%7B%22loglvl%22%3A%22DEBUG%22%2C%22frmAppData%22%3A%22%2Fcloud_py_api%22%2C%22dbConfig%22%3A%7B%22dbType%22%3A%22pgsql%22%2C%22dbUser%22%3A%22%22%2C%22dbName%22%3A%22nextcloud%22%7D%7D"
-DUMP_FOLDERS="ls -la /cloud_py_api /cloud_py_api/.local /var/www"
+DUMP_FOLDERS="ls -la /cloud_py_api /cloud_py_api/.local /cloud_py_api/st_python /var/www"
 CLEAR_FOLDERS="rm -rf /cloud_py_api/.local"
+AS_USER="sudo -u apache"
 
 cp -r host/home/runner/work/cloud_py_api/cloud_py_api/pyfrm /
 if [ -d "/cloud_py_api/st_python" ]; then
-  echo "Python standalone present, checking on it."
-  if ! /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
+  $ECHO_LINE_BREAK && echo "Standalone python(user): checking." && $ECHO_LINE_BREAK
+  if ! $AS_USER /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
     $DUMP_FOLDERS
-    $ECHO_LINE_BREAK && echo "Installing" && $ECHO_LINE_BREAK
-    /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
+    $ECHO_LINE_BREAK && echo "Standalone python(user): installing" && $ECHO_LINE_BREAK
+    $AS_USER /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
   fi
-  $DUMP_FOLDERS && $CLEAR_FOLDERS
+  $DUMP_FOLDERS
+  $CLEAR_FOLDERS
 else
   echo "Standalone python not found. Test failed." && exit 102
 fi
