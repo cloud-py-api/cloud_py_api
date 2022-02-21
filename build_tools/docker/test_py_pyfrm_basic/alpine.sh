8b16f0cffd120f34a60918a38cee3f64305199ff
Alexander Piskun <bigcat88@icloud.com>
2022-02-21 21:47:39 +0200

    dev

--- build_tools/docker/test_py_pyfrm_basic/alpine.sh
+++ build_tools/docker/test_py_pyfrm_basic/alpine.sh
@@ -7,9 +7,14 @@ CLEAR_FOLDERS="rm -rf /cloud_py_api/.local"
 AS_USER="sudo -u apache"
 INSTALL_PIP="apk add --no-cache py3-pip"
 INSTALL_PACKAGES="pip3 install pipdeptree pg8000 PyMySQL protobuf SQLAlchemy"
+CLONED_PY_PATH="/cloud_py_api/st_python_upd_test"
+CLONED_PY_INTP="$CLONED_PY_PATH/bin/python3"
+CLONE_PY_ST="$AS_USER cp -r /cloud_py_api/st_python $CLONED_PY_PATH"
+CLONE_DUMP_FOLDERS="ls -la /cloud_py_api /cloud_py_api/.local /cloud_py_api/st_python_upd_test /var/www"
 
 cp -r host/home/runner/work/cloud_py_api/cloud_py_api/pyfrm /
 if [ -d "/cloud_py_api/st_python" ]; then
+  $CLONE_PY_ST
   $ECHO_LINE_BREAK && echo "Standalone python(user): checking." && $ECHO_LINE_BREAK
   if ! $AS_USER /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
     $DUMP_FOLDERS
@@ -20,6 +25,18 @@ if [ -d "/cloud_py_api/st_python" ]; then
   $ECHO_LINE_BREAK && echo "Standalone python(user): updating" && $ECHO_LINE_BREAK
   $AS_USER /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --update --target framework --dev || exit 101
   $DUMP_FOLDERS
+  $CLEAR_FOLDERS
+  $AS_USER $CLONED_PY_INTP -m pip install --no-cache-dir pg8000==1.23.0 PyMySQL==1.0.1 protobuf==3.19.1 || exit 101
+  $ECHO_LINE_BREAK && echo "Standalone(clone) python(user): checking." && $ECHO_LINE_BREAK
+  if ! $AS_USER $CLONED_PY_INTP /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
+    $CLONE_DUMP_FOLDERS
+    $ECHO_LINE_BREAK && echo "Standalone(clone) python(user): installing" && $ECHO_LINE_BREAK
+    $AS_USER $CLONED_PY_INTP /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
+  fi
+  $CLONE_DUMP_FOLDERS
+  $ECHO_LINE_BREAK && echo "Standalone(clone) python(user): updating" && $ECHO_LINE_BREAK
+  $AS_USER $CLONED_PY_INTP /pyfrm/install.py --config "$FRM_CONFIG" --update --target framework --dev || exit 101
+  $CLONE_DUMP_FOLDERS
   $CLEAR_FOLDERS
 elif ! python3 -V; then
   echo "ERROR! System or Standalone python not found. Test failed." && exit 102
