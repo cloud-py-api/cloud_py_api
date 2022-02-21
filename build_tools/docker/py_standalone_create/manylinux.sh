6ff825a03951b6dd4f8dab979a6f868c9791da8e
Alexander Piskun <bigcat88@icloud.com>
2022-02-21 02:18:55 +0200

    dev

--- build_tools/docker/py_standalone_create/manylinux.sh
+++ build_tools/docker/py_standalone_create/manylinux.sh
@@ -7,7 +7,5 @@ git clone https://github.com/mrecachinas/hexhamming.git || exit 106
 git clone https://github.com/bigcat88/pillow_heif.git || exit 106
 /st_python/bin/python3 -m pytest pillow_heif || exit 106
 
-ls -la /host/home/runner/work/
-ls -la /host/home/runner/work/cloud_py_api
 ls -la /host/home/runner/work/cloud_py_api/cloud_py_api
-echo "TEST OK" && cp /*.tar.zst /host/home/runner/work/cloud_py_api/cloud_py_api/pythons/
+echo "TEST OK" && cp /*.tar.zst /host/home/runner/work/cloud_py_api/cloud_py_api/st_pythons/
