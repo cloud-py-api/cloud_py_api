2c718aba1bb409d4098e22aa797a0ef768f51c3d
Alexander Piskun <bigcat88@icloud.com>
2022-02-21 22:17:53 +0200

    dev

--- build_tools/docker/test_py_pyfrm_basic/centos.sh
+++ build_tools/docker/test_py_pyfrm_basic/centos.sh
@@ -20,11 +20,11 @@ if [ -d "/cloud_py_api/st_python" ]; then
     $AS_USER /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
   fi
   $DUMP_FOLDERS
-  $ECHO_LINE_BREAK && echo "Standalone python(user): updating" && $ECHO_LINE_BREAK
-  $AS_USER /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --update --target framework --dev || exit 101
-  $DUMP_FOLDERS
+#  $ECHO_LINE_BREAK && echo "Standalone python(user): updating" && $ECHO_LINE_BREAK
+#  $AS_USER /cloud_py_api/st_python/bin/python3 /pyfrm/install.py --config "$FRM_CONFIG" --update --target framework --dev || exit 101
+#  $DUMP_FOLDERS
   $CLEAR_FOLDERS
-#  $AS_USER $CLONED_PY_INTP -m pip install --no-cache-dir pg8000==1.23.0 PyMySQL==1.0.1 protobuf==3.19.1 || exit 101
+  $AS_USER $CLONED_PY_INTP -m pip install --no-cache-dir pg8000==1.23.0 PyMySQL==1.0.1 protobuf==3.19.1 || exit 101
   $ECHO_LINE_BREAK && echo "Standalone(clone) python(user): checking." && $ECHO_LINE_BREAK
   if ! $AS_USER $CLONED_PY_INTP /pyfrm/install.py --config "$FRM_CONFIG" --check --target framework --dev; then
     $CLONE_DUMP_FOLDERS
@@ -32,9 +32,9 @@ if [ -d "/cloud_py_api/st_python" ]; then
     $AS_USER $CLONED_PY_INTP /pyfrm/install.py --config "$FRM_CONFIG" --install --target framework --dev || exit 101
   fi
   $CLONE_DUMP_FOLDERS
-  $ECHO_LINE_BREAK && echo "Standalone(clone) python(user): updating" && $ECHO_LINE_BREAK
-  $AS_USER $CLONED_PY_INTP /pyfrm/install.py --config "$FRM_CONFIG" --update --target framework --dev || exit 101
-  $CLONE_DUMP_FOLDERS
+#  $ECHO_LINE_BREAK && echo "Standalone(clone) python(user): updating" && $ECHO_LINE_BREAK
+#  $AS_USER $CLONED_PY_INTP /pyfrm/install.py --config "$FRM_CONFIG" --update --target framework --dev || exit 101
+#  $CLONE_DUMP_FOLDERS
   $CLEAR_FOLDERS
 else
   echo "ERROR! System or Standalone python not found. Test failed." && exit 102
