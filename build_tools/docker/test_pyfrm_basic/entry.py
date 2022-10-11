b2f7b1663a446e1dafa42dccd895a281fd79aa9f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-11 10:34:59 +0000

    Adding tarfile member sanitization to extractall()

--- build_tools/docker/test_pyfrm_basic/entry.py
+++ build_tools/docker/test_pyfrm_basic/entry.py
@@ -235,7 +235,29 @@ def init():
     remove(zst_path)
     tar_path = path.join(FRM_APP_DATA, "standalone.tar")
     with tarfile.open(tar_path) as tar:
-        tar.extractall(FRM_APP_DATA)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, FRM_APP_DATA)
     remove(tar_path)
     if path.isdir(ST_PYTHON_DIR) and environ.get("SKIP_ST_PY_CLONED_TESTS", "0") == "0":
         # Clone standalone python.
